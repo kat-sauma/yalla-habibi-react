24cd4290947e84e5f0bcd42b0a95f5b3deda7037
Kat Sauma <katsauma@gmail.com>
2021-02-25 12:57:29 -0800

    made pages but needed to address the back end before moving forward

---
+++ src/Create.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Create extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <label>
+                        <input />
+                    </label>
+                </form>
+            </div>
+        )
+    }
+}
