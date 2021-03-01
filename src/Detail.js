517e3d3d42d6ea3818d7d3f0cc851033b5be7ac4
Kat Sauma <katsauma@gmail.com>
2021-02-28 19:48:42 -0800

    dynamic dropdown for categories on Detail page.

--- src/Detail.js
+++ src/Detail.js
@@ -10,52 +10,57 @@ export default class Detail extends Component {
         description: ' ',
         category_id: 1,
         size: ' ',
-        price: 1
+        price: 1,
+        category_id: 1,
+        categories: []
     }
 
     componentDidMount = async () => {
         const categories = await getCategories();
-        const {
-            category,
-            clothing_id,
-            description,
-            img_url,
-            name,
-            price,
-            size
-        } = await getClothingItem(this.props.match.params.itemId);
+        // const {
+        //     category,
+        //     clothing_id,
+        //     description,
+        //     img_url,
+        //     name,
+        //     price,
+        //     size,
+        //     category_id,
+        //     categor
+        // } = await getClothingItem(this.props.match.params.itemId);
+        const item = await getClothingItem(this.props.match.params.itemId);
 
-        const category_id = getCategoryId({ category }, categories);
+        const category_id = getCategoryId(item, categories);
+        // this.setState({
+        //     category: category,
+        //     clothing_id: clothing_id,
+        //     description: description,
+        //     img_url: img_url,
+        //     name: name,
+        //     price: price,
+        //     size: size,
+        //     category_id: category_id
+        // })
         this.setState({
-            category: category,
-            clothing_id: clothing_id,
-            description: description,
-            img_url: img_url,
-            name: name,
-            price: price,
-            size: size,
-            category_id: category_id
+            ...item,
+            category_id,
+            categories
         })
-
     }
+
+    handleClothingIdChange = (e) => this.setState({ clothing_id: Number(e.target.value) })
 
     handleNameChange = (e) => this.setState({ name: e.target.value })
 
+    handleURLChange = (e) => this.setState({ img_url: e.target.value })
+
     handleDescriptionChange = (e) => this.setState({ description: e.target.value })
+
+    handleCategoryChange = (e) => this.setState({ category_id: Number(e.target.value) })
 
     handleSizeChange = (e) => this.setState({ size: e.target.value })
 
     handlePriceChange = (e) => this.setState({ price: Number(e.target.value) })
-
-    // handleCategoryChange = () => {
-    //     this.setState({
-    //         category:
-    //             this.state.category === false
-    //                 ? true : false
-    //     })
-    // }
-
-    handleCategoryChange = (e) => this.setState({ category: Number(e.target.value) })
 
     handleSubmit = async (e) => {
         e.preventDefault();
@@ -71,12 +76,31 @@ export default class Detail extends Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>
+                        Inventory ID
+                        <input value={this.state.clothing_id} type="number" onChange={this.handleClothingIdChange} />
+                    </label>
+                    <label>
                         Name of Garment
                         <input value={this.state.name} onChange={this.handleNameChange} />
                     </label>
                     <label>
+                        Image
+                        <input value={this.state.img_url} onChange={this.handleURLChange} />
+                    </label>
+                    <label>
                         Description
                         <input value={this.state.description} onChange={this.handleDescriptionChange} />
+                    </label>
+                    <label>
+                        <select value={this.state.category} onChange={this.handleCategoryChange}>
+                            {
+                                this.state.categories.map(category =>
+                                    <option value={category.id} selected={this.state.category_id === category.id}>
+                                        {category.name}
+                                    </option>
+                                )
+                            }
+                        </select>
                     </label>
                     <label>
                         Size
@@ -86,17 +110,7 @@ export default class Detail extends Component {
                         Price
                         <input value={this.state.price} type="number" onChange={this.handlePriceChange} />
                     </label>
-                    {/* <label>
-                        Vintage?
-                        <input value={this.state.category} type="checkbox" onChange={this.handleCategoryChange} />
-                    </label> */}
-                    <label>
-                        <select value={this.state.category} type="number" onChange={this.handleCategoryChange}>
-                            <option value="1">Silk</option>
-                            <option value="2">Vintage Tops</option>
-                            <option value="3">Contemporary Tops</option>
-                        </select>
-                    </label>
+
                     <button>update</button>
                 </form>
             </div>
